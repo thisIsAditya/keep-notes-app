9e4da331ed5d4af1a4809d2dbe2ccf5b9b6bfb7a
Aditya Pandey <pandey.api2k@gmail.com>
2023-08-13 00:46:20 +0530

    Made UI a bit usable

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,12 +1,18 @@
 import SideBar from 'components/SideBar';
+import styled from 'styled-components';
+
+const StyledContent = styled('div')(() => ({
+  position: 'fixed',
+  left: '5vw',
+}));
 
 const Layout = Comp => {
   return (
     <div>
       <SideBar />
-      <div>
+      <StyledContent>
         <Comp />
-      </div>
+      </StyledContent>
     </div>
   );
 };
--- src/redux/reducers/notes.js
+++ src/redux/reducers/notes.js
@@ -1,66 +1,49 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import {
-  createNoteApi,
-  deleteNoteApi,
-  getAllNotesApi,
-  updateNoteApi,
-} from "../../apis/note-apis";
-import { NOTES_FILTER } from "../../constants";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createNoteApi, deleteNoteApi, getAllNotesApi, updateNoteApi } from '../../apis/note-apis';
+import { NOTES_FILTER } from '../../constants';
 
-export const getUserNotes = createAsyncThunk(
-  "notes/getAllNotesApi",
-  async (_, { getState }) => {
-    const notesFilter = getState().notes.notesFilter;
-    const data = await getAllNotesApi(notesFilter);
-    return data;
-  }
-);
+export const getUserNotes = createAsyncThunk('notes/getAllNotesApi', async (_, { getState }) => {
+  const notesFilter = getState().notes.notesFilter;
+  const data = await getAllNotesApi(notesFilter);
+  return data;
+});
 
-export const saveNote = createAsyncThunk(
-  "notes/saveNote",
-  async (noteDetails, { dispatch }) => {
-    const data =
-      noteDetails.id === "dummy"
-        ? await createNoteApi(noteDetails)
-        : await updateNoteApi(noteDetails);
-    dispatch(updateNote(data));
-    return data;
-  }
-);
+export const saveNote = createAsyncThunk('notes/saveNote', async (noteDetails, { dispatch }) => {
+  const data = noteDetails.id === 'dummy' ? await createNoteApi(noteDetails) : await updateNoteApi(noteDetails);
+  dispatch(updateNote(data));
+  return data;
+});
 
-export const deleteNote = createAsyncThunk(
-  "notes/removeNote",
-  async (noteId, { dispatch }) => {
-    if (noteId !== "dummy") {
-      await deleteNoteApi(noteId);
-    }
-    dispatch(removeNote(noteId));
+export const deleteNote = createAsyncThunk('notes/removeNote', async (noteId, { dispatch }) => {
+  if (noteId !== 'dummy') {
+    await deleteNoteApi(noteId);
   }
-);
+  dispatch(removeNote(noteId));
+});
 
 const setDefaultNotesFilter = () => {
   const notesFilterObj = {};
-  Object.keys(NOTES_FILTER).forEach((filterKey) => {
+  Object.keys(NOTES_FILTER).forEach(filterKey => {
     notesFilterObj[filterKey] = NOTES_FILTER[filterKey].value;
   });
   return notesFilterObj;
 };
 
 export const notes = createSlice({
-  name: "notes",
+  name: 'notes',
   initialState: {
     loading: false,
     noteList: [],
     notesFilter: setDefaultNotesFilter(),
   },
   reducers: {
-    addNote: (state, { payload }) => {
+    addNote: state => {
       const note = {
-        id: "dummy",
-        text: "",
-        priority: 3,
+        id: 'dummy',
+        text: '',
+        priority: 0,
       };
-      const index = state.noteList.findIndex((note) => note.id === "dummy");
+      const index = state.noteList.findIndex(note => note.id === 'dummy');
       if (index > -1) {
         state.noteList[index] = note;
       } else {
@@ -68,9 +51,7 @@ export const notes = createSlice({
       }
     },
     updateNote: (state, { payload }) => {
-      const noteIndex = state.noteList.findIndex(
-        (note) => note.id === payload.id
-      );
+      const noteIndex = state.noteList.findIndex(note => note.id === payload.id);
       const note = {
         id: payload.id,
         text: payload.text,
@@ -83,12 +64,12 @@ export const notes = createSlice({
       }
     },
     removeNote: (state, { payload }) => {
-      state.noteList = state.noteList.filter((note) => note.id !== payload);
+      state.noteList = state.noteList.filter(note => note.id !== payload);
     },
   },
   extraReducers(builder) {
     builder
-      .addCase(getUserNotes.pending, (state) => {
+      .addCase(getUserNotes.pending, state => {
         state.loading = true;
       })
       .addCase(getUserNotes.fulfilled, (state, { payload }) => {
@@ -97,7 +78,7 @@ export const notes = createSlice({
         }
         state.loading = false;
       })
-      .addCase(getUserNotes.rejected, (state) => {
+      .addCase(getUserNotes.rejected, state => {
         state.loading = false;
       });
   },
